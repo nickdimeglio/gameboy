f1b40f80dfeca3cd29962004c231e7e3d230a52f
Nick DiMeglio <contact@nickdimeglio.com>
2023-02-13 17:56:47 -0500

    abstracted out display into a new module

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-use minifb::{Key, ScaleMode, Window, WindowOptions};
+#![allow(unused_mut)]
+mod display;
 
-const WIDTH: usize = 320; 
-const HEIGHT: usize = 288;
+use minifb::{Key, ScaleMode, Window, WindowOptions};
+use display::{Display, GameBoyScreen};
+
+const CGB_WIDTH: usize = 320;
+const CGB_HEIGHT: usize = 288;
 
 fn main() {
+    let mut cgb_screen = GameBoyScreen::new();
+    let mut display = Display::new();
+
+    // Initialize emulator window
     let mut window = Window::new(
         "Welcome to the Game Boy!",
-        WIDTH,
-        HEIGHT,
+        CGB_WIDTH,
+        CGB_HEIGHT,
         WindowOptions {
             resize: true,
             scale_mode: ScaleMode::UpperLeft,
@@ -16,15 +24,14 @@ fn main() {
     )
     .expect("Unable to create window...");
 
-    let mut pixels: Vec<u32> = Vec::from([0; WIDTH * HEIGHT]);
-    let mut buffer: Vec<u32> = Vec::from([0; WIDTH * HEIGHT]);
-    let mut size = (0, 0);
-
+    // Begin Fetch-Decode-Execute loop
     while window.is_open() && !window.is_key_down(Key::Escape) {
+
+        // Resize screen if needed
         let new_size = (window.get_size().0, window.get_size().1);
-        if new_size != size {
-            size = new_size;
-            buffer.resize(size.0 * size.1, 0);
+        if new_size != (display.width, display.height) {
+            (display.width, display.height) = new_size;
+            display.resize();
         }
 
         let mut color: u32 = 0x0FFF;
@@ -38,34 +45,12 @@ fn main() {
             _ => (),
         });
 
-        // Update pixels
-        for i in 0..(WIDTH * HEIGHT) {
-            let row = i / WIDTH;
-            let col = i % HEIGHT;
-            if ((row % 32 < 16) & (col % 32 < 16)) | ((row % 32 > 15) & (col % 32 > 15)) {
-                pixels[i] = color;
-            }
-        }
-
-        // Update buffer
-        let buffer_width = size.0;
-        let buffer_height = size.1;
-        for i in 0..(buffer_width * buffer_height) {
-
-            // Get relative x and y for this buffer pixel (0 to 1)
-            let x = (i % buffer_width) as f64 / buffer_width as f64;
-            let y = (i as f64 / buffer_width as f64) / buffer_height as f64;
-
-            // Calculate absolute position for gameboy pixel
-            let row = (y * HEIGHT as f64).floor() as usize;     // round down
-            let col = (x * WIDTH as f64).floor() as usize;      // round down
-            let pos = row * WIDTH + col;
-            buffer[i] = pixels[pos];
-        }
+        // Update display pixels
+        display.update(&cgb_screen);
 
         // Update window
         window
-            .update_with_buffer(&buffer, new_size.0, new_size.1)
+            .update_with_buffer(&display.pixels, display.width, display.height)
             .unwrap();
     }
 }
