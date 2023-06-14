2240b3c7d32c71fc3186f1af3a483f6730be4091
Nick DiMeglio <contact@nickdimeglio.com>
2023-06-14 10:58:30 -0500

    op0x11, test refactor

--- src/gameboy/memory.rs
+++ src/gameboy/memory.rs
@@ -10,12 +10,11 @@ impl Memory {
         }
     }
 
-    pub fn read(&self, address: usize) -> Option<u8> {
-        if address < self.memory.len() {
-            Some(self.memory[address])
-        } else {
-            None
-        } 
+    pub fn read_8(&self, address: usize) -> Option<u8> {
+        match self.memory.get(address) {
+            None => None,
+            Some(n) => Some(*n)
+        }
     }
 
     pub fn write(&mut self, address: usize, val: u8) {
@@ -25,12 +24,15 @@ impl Memory {
     }
 
     pub fn read_16(&mut self, address: usize) -> Option<u16> {
-        match self.read(address) {
+        match self.read_8(address) {
             None => None,
             Some(lo) => {
-                match self.read(address + 1) {
+                match self.read_8(address + 1) {
                     None => None,
-                    Some(hi) => Some(&((hi as u16) << 8) + lo as u16)
+                    Some(hi) => {
+                        println!("hi: {}, lo: {}", hi, lo);
+                        Some((hi as u16) << 8 + lo as u16)
+                    }
                 }
             }
         }
--- src/gameboy/mod.rs
+++ src/gameboy/mod.rs
@@ -37,8 +37,9 @@ impl GameBoy {
                 self.instruction = *instruction;
                 self.registers.inc_pc();
                 match self.instruction {
-                    0x00 => self.op0x01(),
-                    0x01 => self.op0x02(),
+                    0x00 => self.op0x00(),
+                    0x01 => self.op0x01(),
+                    0x11 => self.op0x11(),
                     _ => Err("Invalid instruction"),
                 }
             }
@@ -49,20 +50,55 @@ impl GameBoy {
      *  OPCODES
      *
      */
-    fn op0x01(&mut self) -> Result<u8, &str> {
+    fn op0x00(&mut self) -> Result<u8, &str> {
         // NOP
-        Ok(0x01)
+        Ok(0x00)
     }
 
-    fn op0x02(&mut self) -> Result<u8, &str> {
+    fn op0x01(&mut self) -> Result<u8, &str> {
         // LD BC, d16
-        match self.memory.read_16(self.registers.get_pc()) {
+        match self.rom_read_16(self.registers.get_pc()) {
+            None => Err("ROM read out of bounds"),
+            Some(nn) => {
+                println!("Storing: {} from {}-{}", nn, self.registers.get_pc(),  self.registers.get_pc() + 1);
+                self.registers.inc_pc();
+                self.registers.inc_pc();
+                self.registers.set_bc(nn);
+                Ok(0x01)
+            }
+        }
+    }
+
+    fn op0x11(&mut self) -> Result<u8, &str> {
+        // LD DE, d16
+        match self.rom_read_16(self.registers.get_pc()) {
             None => Err("ROM read out of bounds"),
             Some(nn) => {
                 self.registers.inc_pc();
                 self.registers.inc_pc();
-                self.registers.set_bc(nn);
-                Ok(0x02)
+                self.registers.set_de(nn);
+                Ok(0x11)
+            }
+        }
+    }
+
+    fn rom_read_8(&mut self, address: usize) -> Option<u8> {
+        // Read 8-bit data from ROM
+        match self.rom.get(address) {
+            None => None,
+            Some(n) => Some(*n)
+        }
+    }
+
+    fn rom_read_16(&mut self, address: usize) -> Option<u16> {
+        // Read 16-bit data from ROM
+        match self.rom_read_8(address) {
+            None => None,
+            Some(lo) => {
+                match self.rom_read_8(address + 1) {
+                    None => None,
+                    Some(hi) => Some(((hi as u16) << 8) + lo as u16)
+                }
             }
         }
     }
